47a230b6a43765e6de00369941f80daebd2c4e75
MidnightEnigma <47158574+MidnightEnigma@users.noreply.github.com>
2019-04-30 06:17:38 -0500

    Add files via upload

---
+++ MuiscApp/gradlew.bat
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="AndroidLayouts">
+    <shared>
+      <config />
+    </shared>
+  </component>
+  <component name="ChangeListManager">
+    <list default="true" id="41ff6b35-18eb-4a64-9929-fb1cd813e8fc" name="Default Changelist" comment="">
+      <change afterPath="$PROJECT_DIR$/app/secret.properties" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/java/edu/illinois/cs/cs125/spring2019/mp3/MainActivity.java" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/java/edu/illinois/cs/cs125/spring2019/mp3/MainActivity.java" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp3/lib/RecognizePhoto.java" beforeDir="false" afterPath="$PROJECT_DIR$/lib/src/main/java/edu/illinois/cs/cs125/spring2019/mp3/lib/RecognizePhoto.java" afterDir="false" />
+    </list>
+    <option name="EXCLUDED_CONVERTED_TO_IGNORED" value="true" />
+    <option name="SHOW_DIALOG" value="false" />
+    <option name="HIGHLIGHT_CONFLICTS" value="true" />
+    <option name="HIGHLIGHT_NON_ACTIVE_CHANGELIST" value="false" />
+    <option name="LAST_RESOLUTION" value="IGNORE" />
+  </component>
+  <component name="ExternalProjectsManager">
+    <system id="GRADLE">
+      <state>
+        <task path="$PROJECT_DIR$">
+          <activation />
+        </task>
+        <projects_view />
+      </state>
+    </system>
+  </component>
+  <component name="FUSProjectUsageTrigger">
+    <session id="1657765768">
+      <usages-collector id="statistics.lifecycle.project">
+        <counts>
+          <entry key="project.closed" value="2" />
+          <entry key="project.open.time.0" value="1" />
+          <entry key="project.open.time.10" value="1" />
+          <entry key="project.open.time.12" value="1" />
+          <entry key="project.open.time.7" value="2" />
+          <entry key="project.opened" value="5" />
+        </counts>
+      </usages-collector>
+      <usages-collector id="statistics.file.extensions.open">
+        <counts>
+          <entry key="class" value="1" />
+          <entry key="java" value="9" />
+          <entry key="properties" value="2" />
+          <entry key="txt" value="1" />
+        </counts>
+      </usages-collector>
+  
